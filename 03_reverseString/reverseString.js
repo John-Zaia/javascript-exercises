1378c1c829f25b8a247e07f67c599492b6f22d1c
John Zaia <113467748+John-Zaia@users.noreply.github.com>
2024-01-13 23:50:38 -0500

    complete 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(reverseWord) {
+    let myArray = reverseWord.split("");
+    let newArray = myArray.reverse();
+    let formattedWord = '';
 
+    for (i in newArray){
+        formattedWord += newArray[i];
+    }
+
+    return formattedWord;
 };
+
+let word = "hello";
+reverseString(word);
 
 // Do not edit below this line
 module.exports = reverseString;
