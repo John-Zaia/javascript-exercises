8ad0a2531f1f7d7e5b4b4c235d5780ba380722b1
John Zaia <113467748+John-Zaia@users.noreply.github.com>
2024-01-13 23:36:54 -0500

    complete 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    let repeatWord = '';
 
+    if (num < 0){
+        return repeatWord = "ERROR";
+    }
+    
+    for (i = 0; i < num; i++){
+        repeatWord += word;
+    }
+
+    return repeatWord;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
