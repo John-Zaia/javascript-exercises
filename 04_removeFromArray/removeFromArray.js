a23ffb49f5170fd7d5b7c07a2fe1189ec1f2161f
John Zaia <113467748+John-Zaia@users.noreply.github.com>
2024-01-15 21:38:48 -0500

    complete 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,  ...args) {
 
-};
+    for (let i = array.length - 1; i >= 0; i--){
+        if (args.includes(array[i])){
+            array.splice(i,1);
+        }
+    }
+    
+    return array
+}
+
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
 
 // Do not edit below this line
 module.exports = removeFromArray;
