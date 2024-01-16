50a8799437783c2fba953963aec9f518d93fb560
John Zaia <113467748+John-Zaia@users.noreply.github.com>
2024-01-15 22:27:26 -0500

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if(num1 < 0 || num2 < 0 || typeof num1 !== "number" || typeof num2 !== "number"){
+        return "ERROR"
+    }
+    else if(num2 > num1){
+        for (let i = num1; i <= num2; i++){
+            sum += i;
+        }
+    }
+    else if(num1 > num2){
+        for (let i = num2; i <= num1; i++){
+            sum += i;
+        }
+    }
 
+    return sum
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
